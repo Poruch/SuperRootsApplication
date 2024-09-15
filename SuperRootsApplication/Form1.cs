1284952d9bdda4947c90147aee3be4875b4b76bd
Александр <sura14081976@gmail.com>
2024-09-15 19:32:55 +0500

    add RootCalculator

--- SuperRootsApplication/Form1.cs
+++ SuperRootsApplication/Form1.cs
@@ -13,20 +13,7 @@ namespace SuperRootsApplication
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int a = trackBar1.Value;
-            string result = "";                  
-            string s = Math.Pow(Double.Parse(textBox1.Text), 1 / Double.Parse(textBox2.Text)).ToString();
-            
-            int doti = s.IndexOf(',');
-            if (s.Contains(',') && a != 0)
-                result = s.Substring(0, doti) + s.Substring(doti, a + 1);
-            else
-                if (doti == -1)
-                result = s;
-            else
-                result = s.Substring(0, doti);
-            Console.WriteLine(result);
-            textBox1.Text = result;
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)

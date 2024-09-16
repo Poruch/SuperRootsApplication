ab018dca5220130cb98481edd729b0ccaa24f025
Александр <sura14081976@gmail.com>
2024-09-16 18:11:44 +0500

    Add class for language

---
+++ SuperRootsApplication/LanguageLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace SuperRootsApplication
+{
+    internal class LanguageLoader
+    {
+        static public void SetTranslation(string file)
+        {
+            StreamReader f = File.OpenText(file);
+            //while (!f.EndOfStream) 
+            //{ 
+            //    //
+            //    //
+            //    //
+            //}
+        }        
+    }
+}
--- SuperRootsApplication/RootCalculator.cs
+++ SuperRootsApplication/RootCalculator.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SuperRootsApplication
 {
@@ -12,17 +8,20 @@ namespace SuperRootsApplication
         {
             return Math.Round(Math.Sqrt(x), precision);
         }
-        public Tuple<double, double> ISqrt(double real, double imaginary, double n, int precision = 15)
+        public Tuple<double, double> ISqrt(double real, double imaginary, int precision = 15)
         {
             double r = Math.Sqrt(real * real + imaginary * imaginary);
             double cos = real / r;
             double f = Math.Acos(cos);
-            f = f / n;
+            f = f / 2;
             return new Tuple<double, double>(
-                Math.Round(Math.Pow(r, 1.0 / n) * Math.Cos(f), precision),
-                Math.Round(Math.Pow(r, 1.0 / n) * Math.Sin(f), precision));
+                Math.Round(Math.Pow(r, 0.5f) * Math.Cos(f), precision),
+                Math.Round(Math.Pow(r, 0.5f) * Math.Sin(f), precision));
         }
-
+        public string ANALRoot()
+        {
+            return "";
+        }
 
     }
 }
